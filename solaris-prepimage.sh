2cb1a606f148ee702f7ac04c37a3637d853e54c6
Matt Ingenthron <ingenthr@cep.net>
2009-07-29 02:15:02 -0700

    updates to image and set architecture name, removed /etc/system changes

--- solaris-prepimage.sh
+++ solaris-prepimage.sh
@@ -1,7 +1,6 @@
 #!/bin/ksh
 
-echo This is just notes at this point
-exit -1
+set -xe
 
 export JAVA_HOME=/usr/jdk/latest
 export EC2_HOME=/opt/ec2
@@ -13,9 +12,13 @@ export EC2_PRIVATE_KEY=/mnt/keys/pk-WBDFRTOLJOOKKAESM24HXVVCW556MAYH.pem
 export EC2_CERT=/mnt/keys/cert-WBDFRTOLJOOKKAESM24HXVVCW556MAYH.pem
 #export EC2_KEYID=blah
 #export EC2_KEY=blah
+export EC2_ACCT_NUM=7860-1448-3886
 export DIRECTORY=/mnt 
-export IMAGE=mrroboto.img
-
+if [[ `isalist | cut -f 1 -d " "` == "amd64" ]]; then
+  export IMAGE=community-memcached-1.4.0+instrumentation-x86_64
+else
+  export IMAGE=community-memcached-1.4.0+instrumentation-i386
+fi
 
 if [[ ! -f $EC2_PRIVATE_KEY || ! -f $EC2_CERT ]]; then
   echo keys required for rebundling and password required to get git
@@ -48,14 +51,6 @@ dmake install
 fi
 
 
-# it's a hack, but get a working git from a 2009.06 box
-#tar -czvf /var/tmp/gitfor2008.11.tar.gz `pkg contents -t file \
-# -o path SUNWgit | grep -v PATH`
-#cd /
-#wget http://blogs.ingenthron.org/gitfor2008.11.tar.gz
-#tar -zxf gitfor2008.11.tar.gz
-#rm gitfor2008.11.tar.gz
-
 # get the python stuff
 PATH=/opt/SunStudioExpress/bin:$PATH
 cd /tmp
@@ -76,7 +71,7 @@ TDFROOT=/var/tdf
 mkdir -p $TDFROOT
 cp -r tdf /usr/local
 rm -rf /usr/local/tdf/.git
-cd /tmp/src/mrroboto/imagebuild
+cd /tmp/src/mrroboto
 cp -r dscripts /usr/local/tdf
 cd /tmp/src/mrroboto
 cp tdf/tdf-svc /usr/local/tdf
@@ -84,49 +79,58 @@ cp /usr/local/tdf/tdf.mrroboto /usr/local/tdf/tdf.tac
 svccfg import tdf/tdf-manifest.xml
 
 # set up automatic startup and verify services start
-cp memcached-svc /usr/local/bin
+cp imagebuild/memcached-svc /usr/local/bin
 
 # cap ZFS memory usage
-cp /etc/system /etc/system.bak
-cat << STOP >> /etc/system
-*
-* lower the memory consumed by ZFS, since we won't be doing much FS stuff
-* here
-*
-* http://www.solarisinternals.com/wiki/index.php/ZFS_Evil_Tuning_Guide#Limiting_the_ARC_Cache
-*
-set zfs:zfs_arc_max = 1073741824
-
-STOP
+# this may not work on EC2 :(
+#cp /etc/system /etc/system.bak
+#cat << STOP >> /etc/system
+#*
+#* lower the memory consumed by ZFS, since we won't be doing much FS stuff
+#* here
+#*
+#* http://www.solarisinternals.com/wiki/index.php/ZFS_Evil_Tuning_Guide#Limiting_the_ARC_Cache
+#*
+#set zfs:zfs_arc_max = 1073741824
+#
+#STOP
 
 # set up lighttpd
-/tmp/src/mrroboto/gen-htdigest.sh > /var/lighttpd/1.4/lighttpd-htdigest.user
+/tmp/src/mrroboto/imagebuild/gen-htdigest.sh > /var/lighttpd/1.4/lighttpd-htdigest.user
 chmod 600 /var/lighttpd/1.4/lighttpd-htdigest.user
 chown webservd:webservd /var/lighttpd/1.4/lighttpd-htdigest.user
-patch /etc/lighttpd/1.4/lightpd.conf /tmp/src/mrroboto/imagebuild/lighttpd.conf.patch
+patch /etc/lighttpd/1.4/lighttpd.conf /tmp/src/mrroboto/imagebuild/lighttpd.conf.patch
 svcadm enable http:lighttpd14
 
 
 # do the rebundling itself
-rm -r /root/.ssh
+rm -rf /root/.ssh
 rm -f /var/adm/messages.[01234]
 > /var/adm/messages
 > /var/adm/utmpx
 > /var/adm/wtmpx
-rm /root/.bash_history
+rm -f /root/.bash_history
+
+cd /mnt 
+if [[ ! -f $IMAGE ]]; then
+  /opt/ec2/sbin/rebundle.sh -v -y $IMAGE
+fi
+
+mkdir -p $DIRECTORY/parts
+mkdir -p $DIRECTORY/keys
 
 if [[ `isalist | cut -f 1 -d " "` == "amd64" ]]; then
   ec2-bundle-image -c $EC2_CERT -k $EC2_PRIVATE_KEY   \
     --kernel aki-fb3ddc92 --ramdisk ari-fd3ddc94 \
     --block-device-mapping "root=rpool/56@0,ami=0,ephemeral0=1,ephemeral1=2,ephemeral2=3,ephemeral3=4" \
-    --user <user-id> --arch x86_64 \
+    --user $EC2_ACCT_NUM --arch x86_64 \
     -i $DIRECTORY/$IMAGE -d $DIRECTORY/parts 
 else
   cd $DIRECTORY
   ec2-bundle-image -c $EC2_CERT -k $EC2_PRIVATE_KEY   \
     --kernel aki-1783627e --ramdisk ari-858362ec \
     --block-device-mapping "root=rpool/56@0,ami=0,ephemeral0=1" \
-    --user <user-id> --arch i386 \
+    --user $EC2_ACCT_NUM --arch i386 \
     -i $DIRECTORY/$IMAGE -d $DIRECTORY/parts
 fi
 
---
+++ solaris-registerimage.sh
@@ -0,0 +1,30 @@
+#!/bin/ksh
+
+set -xe
+
+export JAVA_HOME=/usr/jdk/latest
+export EC2_HOME=/opt/ec2
+export BUCKET=northscale
+export PATH=$PATH:$EC2_HOME/bin 
+export RUBYLIB=$EC2_HOME/lib 
+export EC2_URL=https://ec2.amazonaws.com 
+export EC2_PRIVATE_KEY=/mnt/keys/pk-WBDFRTOLJOOKKAESM24HXVVCW556MAYH.pem
+export EC2_CERT=/mnt/keys/cert-WBDFRTOLJOOKKAESM24HXVVCW556MAYH.pem
+export EC2_KEYID=AKIAJ3BZHH4WP3ZD3RBQ
+export EC2_KEY=vFkeaVabtCV2R9j/U+EF7QJRNAfeBS57cFZQ7zgJ
+export EC2_ACCT_NUM=7860-1448-3886
+export DIRECTORY=/mnt 
+if [[ `isalist | cut -f 1 -d " "` == "amd64" ]]; then
+  export IMAGE=community-memcached-1.4.0+instrumentation-x86_64
+else
+  export IMAGE=community-memcached-1.4.0+instrumentation-i386
+fi
+export BUCKET=northscale
+
+
+if [[ ! -f $EC2_PRIVATE_KEY || ! -f $EC2_CERT ]]; then
+  echo keys required for rebundling and password required to get git
+  exit -1
+fi
+
+ec2reg -C $EC2_CERT -K $EC2_PRIVATE_KEY $BUCKET/$IMAGE.manifest.xml 
