e380773096557cfe32c1ebb8467bde6c2b3b1eb7
Matt Ingenthron <ingenthr@cep.net>
2009-08-31 22:55:54 -0700

    Updated services, updated twistd-setup, removal of memcached as submodule.

--- imagebuild/twistd-setup.sh
+++ imagebuild/twistd-setup.sh
@@ -16,8 +16,8 @@ fi
 mkdir -p /opt/northscale/lib/python2.5/site-packages
 env PATH=$PATH CC=$CC CXX=$CXX PYTHONPATH=$PYTHONPATH \
   /usr/gnu/bin/sh setuptools-0.6c9-py2.5.egg --prefix=$NSCALEPREFIX
-env PYTHONPATH=$PYTHONPATH $NSCALEPREFIX/bin/easy_install Twisted
-env PYTHONPATH=$PYTHONPATH $NSCALEPREFIX/bin/easy_install simplejson
+env PYTHONPATH=$PYTHONPATH $NSCALEPREFIX/bin/easy_install --prefix=$NSCALEPREFIX Twisted
+env PYTHONPATH=$PYTHONPATH $NSCALEPREFIX/bin/easy_install --prefix=$NSCALEPREFIX simplejson
 
 cp -R tdf /opt/northscale
 rm -rf /opt/northscale/tdf/.git
