7281ecb86e329b469a02383763db775ce0af585a
Matt Ingenthron <ingenthr@cep.net>
2009-09-05 04:39:58 -0700

    Added custom build to pkg directory for twistd.

--- imagebuild/masterbuild.sh
+++ imagebuild/masterbuild.sh
@@ -3,7 +3,11 @@
 set -xe
 
 CC=/opt/SunStudioExpress/bin/cc
+CXX=/opt/SunStudioExpress/bin/CC
 PATH=/opt/SunStudioExpress/bin:/usr/gnu/bin:/usr/bin:/usr/sbin:/sbin
 
 echo Building memcached...
-env CC=$CC PATH=$PATH && memcached/build.sh $@
+env CC=$CC PATH=$PATH memcached/build.sh $@
+
+echo Building twistd...
+env CC=$CC CXX=$CXX PATH=$PATH twistd/build.sh $@
--- imagebuild/twistd/twistd-setup.sh
+++ imagebuild/twistd/twistd-setup.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/ksh 
 
+set -xe
+
 # get easy install and then twistd and simplejson
-PATH=/opt/SunStudioExpress/bin:/usr/gnu/bin:/usr/bin
-CC=/opt/SunStudioExpress/bin/cc
-CXX=/opt/SunStudioExpress/bin/CC
-NSCALEPREFIX=/opt/northscale
-PYTHONPATH=$NSCALEPREFIX/lib/python2.5/site-packages
+# path vars should come from execution environment
+#PATH=/opt/SunStudioExpress/bin:/usr/gnu/bin:/usr/bin
+#CC=/opt/SunStudioExpress/bin/cc
+#CXX=/opt/SunStudioExpress/bin/CC
+#INSTLOC=/opt/northscale
+PYTHONPATH=$INSTLOC/lib/python2.5/site-packages
+
+if [[ -n $PYTHONPATH ]]; then
+  if ! mkdir -p $PYTHONPATH; then
+    echo $PYTHONPATH does not exist and could not create it
+    exit -1
+  fi
+fi
 
 # depends on SUNWPython25
 #get easy install
@@ -15,9 +25,9 @@ fi
 
 mkdir -p /opt/northscale/lib/python2.5/site-packages
 env PATH=$PATH CC=$CC CXX=$CXX PYTHONPATH=$PYTHONPATH \
-  /usr/gnu/bin/sh setuptools-0.6c9-py2.5.egg --prefix=$NSCALEPREFIX
-env PYTHONPATH=$PYTHONPATH $NSCALEPREFIX/bin/easy_install --prefix=$NSCALEPREFIX Twisted
-env PYTHONPATH=$PYTHONPATH $NSCALEPREFIX/bin/easy_install --prefix=$NSCALEPREFIX simplejson
+  /usr/gnu/bin/sh setuptools-0.6c9-py2.5.egg --prefix=$INSTLOC
+env PYTHONPATH=$PYTHONPATH $INSTLOC/bin/easy_install --prefix=$INSTLOC Twisted
+env PYTHONPATH=$PYTHONPATH $INSTLOC/bin/easy_install --prefix=$INSTLOC simplejson
 
-cp -R tdf /opt/northscale
-rm -rf /opt/northscale/tdf/.git
+#cp -R tdf /opt/northscale
+#rm -rf /opt/northscale/tdf/.git
