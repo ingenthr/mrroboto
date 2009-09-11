d77413e48713595c07722c0873ebfdc11258242d
Matt Ingenthron <mi109165@corea.(none)>
2009-09-11 11:25:54 -0700

    New package scripts for ec2lighttpdauth service.

---
+++ imagebuild/pkg_ec2lighttpdauth.sh
@@ -0,0 +1,19 @@
+#!/bin/ksh
+
+#
+# Load the ec2lighttpd auth service to the pkg repo
+#
+
+set -xe
+
+. pkg.env
+
+
+eval `${pkgsendcmd} open ec2lighttpdauth@1.0-1`
+${pkgsendcmd} add dir mode=0555 owner=root group=bin path=/opt/northscale
+${pkgsendcmd} add file gen-htdigest.sh mode=0555 owner=root group=bin \
+path=/opt/northscale/gen-htdigest.sh
+${pkgsendcmd} add file ec2lighttpdauth.xml mode=055 owner=root group=bin \
+path=/var/svc/manifest/application/ec2lighttpdauth.xml
+${pkgsendcmd} add set restart_fmri=svc:/system/manifest-import:default 
+${pkgsendcmd} close
